6b17e8b17d2858e16600202c035fec756c98ebfb
Brendan Hill <brendan.hill3@my.jcu.edu.au>
2018-08-21 08:17:28 +1000

    Refactor program to use functions

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,9 +1,16 @@
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    score = float(input("Enter score: "))
+    result = calculate_result(score)
+    print(result)
+
+def calculate_result(score):
+    if score < 0 or score > 100:
+        return ("Invalid score")
+    elif score >= 90:
+        return ("Excellent")
+    elif score >= 50:
+        return ("Passable")
+    else:
+        return ("Bad")
+
+main()
