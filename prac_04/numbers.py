f257961c24718b2fe849a2ad75946d0431cee202
Brendan Hill <brendan.hill3@my.jcu.edu.au>
2018-08-27 19:44:07 +1000

    Reformat code to match PEP8 standards

--- prac_04/numbers.py
+++ prac_04/numbers.py
@@ -1,7 +1,7 @@
 def main():
     numbers = []
     total = 0
-    for i in range (0,5):
+    for i in range(0, 5):
         number = int(input("Number: "))
         numbers.append(number)
         total += number
@@ -9,6 +9,7 @@ def main():
     print("The last number is {}".format(numbers[-1]))
     print("The smallest number is {}".format(min(numbers)))
     print("The largest number is {}".format(max(numbers)))
-    print("The average of the numbers is {}".format(sum(numbers)/len(numbers)))
+    print("The average of the numbers is {}".format(sum(numbers) / len(numbers)))
+
 
 main()
