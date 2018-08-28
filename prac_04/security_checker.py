c42e1577c43c0bb5bd810011d9f143a1a166201a
Brendan Hill <brendan.hill3@my.jcu.edu.au>
2018-08-28 14:05:35 +1000

    Rename file to match prac requirements

--- prac_04/security_checker.py
+++
@@ -1,7 +0,0 @@
-usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
-             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
-username = input("Enter your username: ")
-if username in usernames:
-    print("Access Granted")
-else:
-    print("Access Denied")
