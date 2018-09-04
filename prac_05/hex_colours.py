691f29a681eb80ffabe4085895c8bdf050aeba50
Brendan Hill <brendan.hill3@my.jcu.edu.au>
2018-09-04 11:31:04 +1000

    Remove for loop and print statement from program

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,9 +1,6 @@
 HEX_COLOURS = {"Brown": "#a52a2a", "Grey": "#bebebe", "White": "#ffffff", "Black": "#000000", "Blue":
                 "#0000ff", "Yellow": "#ffff00", "Pink": "#ffc0cb", "Green": "#00ff00", "Red": "#ff0000",
                "Orange": "#ffa500", "Purple": "#a020f0", }
-for colour in HEX_COLOURS:
-    print("{:6} is {}".format(colour, HEX_COLOURS[colour]))
-
 colour = input("Enter colour name: ").title()
 while colour != "":
     if colour in HEX_COLOURS:
