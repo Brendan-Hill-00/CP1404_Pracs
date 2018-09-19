23f0893ce7b93ce329e2fda43562d2d35fb6e326
Brendan Hill <brendan.hill3@my.jcu.edu.au>
2018-09-19 21:46:24 +1000

    Complete code to meet stage 2 requirements

--- prac_07/distance_converter.py
+++ prac_07/distance_converter.py
@@ -13,14 +13,22 @@ class DistanceConverterApp(App):
         return self.root
 
     def handle_increment(self, value):
-        new_value = int(self.root.ids.input_number.text) + value
+        new_value = self.get_valid_input() + value
         self.root.ids.input_number.text = str(new_value)
-        self.handle_calculate(int(new_value))
+        self.handle_calculate()
 
 
-    def handle_calculate(self, value):
-        converted_distance = value * MILES_TO_KM
+    def handle_calculate(self):
+        converted_distance = self.get_valid_input() * MILES_TO_KM
         self.root.ids.output_label.text = str(converted_distance)
+
+    def get_valid_input(self):
+        try:
+            value = float(self.root.ids.input_number.text)
+            return value
+        except ValueError:
+            return 0
+
 
 
 DistanceConverterApp().run()
