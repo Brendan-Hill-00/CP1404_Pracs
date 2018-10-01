328aa312775a6d6b857c0038a5b0c4b98a1da1b2
Brendan Hill <brendan.hill3@my.jcu.edu.au>
2018-10-02 09:26:44 +1000

    Rewrite the Python code to run without Kivy button interaction

--- prac_07/dynamic_labels.py
+++ prac_07/dynamic_labels.py
@@ -2,16 +2,16 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.lang import Builder
 
+
 class DynamicLabelsApp(App):
     def build(self):
         self.title = "Dynamic Labels App"
         self.root = Builder.load_file('dynamic_labels.kv')
-        return self.root
-
-    def create_labels(self):
-        self.names = ["Clark", "Veronica", "Noah", "Patty"]
+        self.names = ["Clark", "Veronica", "Noah", "Patty", "Dave"]
         for name in self.names:
             temp_label = Label(text=name, id=name)
             self.root.ids.placeholder.add_widget(temp_label)
+        return self.root
+
 
 DynamicLabelsApp().run()
