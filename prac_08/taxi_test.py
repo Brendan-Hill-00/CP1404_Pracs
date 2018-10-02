689694012a8be0cf8b2b88ac46c3e14cb895b83a
Brendan Hill <brendan.hill3@my.jcu.edu.au>
2018-10-02 14:47:22 +1000

    Refactor the code to have price_per_km as a class variable

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -3,7 +3,7 @@ from prac_08.taxi import Taxi
 
 def main():
     print("Taxi Testing")
-    new_taxi = Taxi("Prius 1", 100, 1.23)
+    new_taxi = Taxi("Prius 1", 100)
     new_taxi.drive(40)
     print(new_taxi)
     print("The price of that trip was {}".format(new_taxi.get_fare()))
