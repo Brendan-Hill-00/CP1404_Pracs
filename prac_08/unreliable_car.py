8239d1e16d4739f7849d7e16fa008b809a48c88b
Brendan Hill <brendan.hill3@my.jcu.edu.au>
2018-10-16 09:47:54 +1000

    Edit comments to better reflect the code

--- prac_08/unreliable_car.py
+++ prac_08/unreliable_car.py
@@ -10,7 +10,6 @@ class UnreliableCar(Car):
     """Specialised version of a Car that includes reliability."""
 
     def __init__(self, name, fuel, reliability):
-        """Initialise a Taxi instance, based on parent class Car."""
         super().__init__(name, fuel)
         self.reliability = reliability
 
