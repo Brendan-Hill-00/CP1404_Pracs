064049a78c25b675f94490401fdb39f10251d7b3
Brendan Hill <brendan.hill3@my.jcu.edu.au>
2018-10-23 14:45:11 +1000

    Complete the create a pyramid recursive functions

--- prac_10/recursion.py
+++ prac_10/recursion.py
@@ -16,6 +16,7 @@ Expected output:
 1 + 0 + 1 + 0 + 1 = 3
 """
 
+print(do_it(5))
 
 def do_something(n):
     """Print the squares of positive numbers from n down to 0."""
@@ -30,3 +31,18 @@ Expected output
 Endlessly printing the squares of all the numbers from -1 onwards
 """
 do_something(4)
+
+
+def calculate_blocks(rows):
+    if rows <= 0:
+        return 0
+    return rows + calculate_blocks(rows - 1)
+
+
+def build_pyramid():
+    number_of_rows = int(input("Would you like to build a pyramid? Enter a number of rows: "))
+    print("To build a pyramid {} rows tall, you will need {} blocks".format(number_of_rows,
+                                                                            calculate_blocks(number_of_rows)))
+
+
+build_pyramid()
